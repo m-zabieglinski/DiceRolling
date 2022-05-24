3f227adc6c201c9018ae3660045f59f4c9bb1047
m-zabieglinski <100228539+m-zabieglinski@users.noreply.github.com>
2022-05-24 17:11:47 +0200

    Add files via upload

--- dicerolling.py
+++ dicerolling.py
@@ -1,13 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+#diceroll rolls n d-sided dice and returns the sum of their results
 def diceroll(n = 1, d = 6):
     sum = 0
     for i in range(n):
         sum = sum + np.random.randint(1, d+1)
     return sum
 
+#mixroll combines multiple dicerolls: n and d are now lists of number and type of dice
+#for example, mixroll(n = [2,5], d = [6, 7]) will roll 2 6-sided die and 5 7-sided die
 def mixroll(n = [1], d = [6]):
+    n = [n]
+    d = [d]
     if len(n) != len(d):
         print("ERROR: input the list of the number of each dice thrown and the type of each dice thrown. lists must be same length")
     else:
@@ -17,6 +22,8 @@ def mixroll(n = [1], d = [6]):
                 sum = sum + np.random.randint(1, d[k]+1)
         return sum
 
+#diceprobtable does the same diceroll many times (def 20k) and creates a probability table of each value
+#it returns a dictionary
 def diceprobtable(n = 1, d = 6, N = 20000):
     val = list(range(n, n*d+1))
     freq = [0] * (d * n)
@@ -28,6 +35,9 @@ def diceprobtable(n = 1, d = 6, N = 20000):
         dictionary[val] = dictionary[val] / N        
     return dictionary;
 
+#DiceRollDist creates a class that represents a singular diceroll
+#method tables returns diceprobtable for that roll
+#method plot plots that diceroll table as neat graph
 class DiceRollDist():
     
     def __init__(self, NumberOfDice, NumberOfSides, NumberOfRolls = 20000):
