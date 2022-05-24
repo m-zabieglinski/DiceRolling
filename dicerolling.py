063ed42674764a79575331afb4761c14dcab4df1
m-zabieglinski <100228539+m-zabieglinski@users.noreply.github.com>
2022-05-24 16:59:29 +0200

    Add files via upload

---
+++ dicerolling.py
@@ -0,0 +1,74 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def diceroll(n = 1, d = 6):
+    sum = 0
+    for i in range(n):
+        sum = sum + np.random.randint(1, d+1)
+    return sum
+
+def mixroll(n = [1], d = [6]):
+    if len(n) != len(d):
+        print("ERROR: input the list of the number of each dice thrown and the type of each dice thrown. lists must be same length")
+    else:
+        sum = 0
+        for k in range(len(n)):
+            for i in range(n[k]):
+                sum = sum + np.random.randint(1, d[k]+1)
+        return sum
+
+def diceprobtable(n = 1, d = 6, N = 20000):
+    val = list(range(n, n*d+1))
+    freq = [0] * (d * n)
+    dictionary = {val[i]: freq[i] for i in range(len(val))}
+    for i in range(N):
+        temp_roll = diceroll(n, d)
+        dictionary[temp_roll] += 1
+    for val in dictionary:
+        dictionary[val] = dictionary[val] / N        
+    return dictionary;
+
+class DiceRollDist():
+    
+    def __init__(self, NumberOfDice, NumberOfSides, NumberOfRolls = 20000):
+        self.n = NumberOfDice
+        self.d = NumberOfSides
+        self.N = NumberOfRolls
+        self.table = diceprobtable(NumberOfDice, NumberOfSides, NumberOfRolls)
+        
+    def table(self):
+        self.table
+    
+    def plot(self, dpi = 300):
+        
+        plt.rcParams['figure.dpi'] = dpi
+        plt.rcParams['savefig.dpi'] = dpi
+        
+        Val = list(self.table.keys())
+        Freq = list(self.table.values())
+        
+        median = sum(list(range(1, 7))) / self.d * self.n
+        
+        plt.scatter(Val, Freq, color = "red")
+        plt.ylim([0, 1.2 * max(Freq)])
+        plt.xlim([min(Val), max(Val)])
+        
+        Val_ticks = np.array([median] * 9)
+        
+        scale = 4
+        for i in range(scale):
+            Val_ticks[i] = i/scale * Val_ticks[i] + (1-i/scale) * min(Val)
+            Val_ticks[i + scale + 1] = i/scale * Val_ticks[i + scale + 1] + (1-i/scale) * max(Val)
+        
+        
+        plt.xticks(Val_ticks)
+                     
+        plt.xlabel("Value on the dice")
+        plt.ylabel("Frequency")
+        plt.title(f"Value distribution for {self.n} {self.d}-sided dice thrown {self.N} times")
+        
+        plt.vlines(x = median, ymin = 0, ymax = max(Freq), colors = 'black', linestyles = '--', linewidth = 5)
+        
+        plt.grid()
+        
+        plt.show()
